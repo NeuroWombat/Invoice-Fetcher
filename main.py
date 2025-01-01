a930d01010a984cda5d21de09bd35675688373e7
Kamil Kula <xkamil580@gmail.com>
2025-01-01 01:20:06 +0100

    added limit_selector and changing time_period for invoices

--- main.py
+++ main.py
@@ -4,26 +4,26 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import time
 
-driver=webdriver.Firefox()
+driver = webdriver.Firefox()
 
 
-def login(email,password):
+def login(email, password):
     driver.get('https://app.autopay.eu/login')
 
     # Accepting cookies to input user data and press login button
-    driver.find_element(By.ID,'cmpwelcomebtnyes').click()
-    time.sleep(1) # Wait for cookies pop-up to disappear
+    driver.find_element(By.ID, 'cmpwelcomebtnyes').click()
+    time.sleep(1)  # Wait for cookies pop-up to disappear
 
-    email_input=driver.find_element(By.ID,'email')
-    pass_input=driver.find_element(By.ID,'password')
-    submit_BTN=driver.find_element(By.XPATH,"//button[@type='submit']")
+    email_input = driver.find_element(By.ID, 'email')
+    pass_input = driver.find_element(By.ID, 'password')
+    submit_BTN = driver.find_element(By.XPATH, "//button[@type='submit']")
 
     email_input.send_keys(email)
     pass_input.send_keys(password)
     submit_BTN.click()
 
     # Checking if login was successful by changing url to dashboard
-    time.sleep(2) # Wait for change of site
+    time.sleep(2)  # Wait for change of site
     if "dashboard" in driver.current_url:
         return True
     else:
@@ -35,44 +35,67 @@ def login(email,password):
 def logoutClose():
     driver.get('https://app.autopay.eu/dashboard')
 
-    logout_BTN=driver.find_element(By.CLASS_NAME,'autopay-header__logout')
+    logout_BTN = driver.find_element(By.CLASS_NAME, 'autopay-header__logout')
     logout_BTN.click()
 
     driver.quit()
 
 
-def getInvoices(mode):
+def getInvoices():
     driver.get('https://app.autopay.eu/invoices')
+    time.sleep(2)  # Wait for site load
 
-    period_mode=driver.find_element(By.CLASS_NAME,'')
-    change_period_BTN=driver.find_element(By.CLASS_NAME,'')
-    select_number=driver.find_element(By.CLASS_NAME,'')
-    pages_number=driver.find_element(By.CLASS_NAME,'')
+    # Setting limit selector to maximum [50]
+    invoice_limit = driver.find_element(By.XPATH,
+                                        '//autopay-select[@name="pageLimit"]//div[@class="ngx-select__toggle btn form-control"]')
+    invoice_limit.click()
+    limit_option = driver.find_element(By.XPATH, '//span[text()="50"]')
+    limit_option.click()
 
-    #Select(period_mode).select_by_value('Current month')
-    #Select(pages_number).select_by_value('50')
+    # Version for time from select
+    time_period = driver.find_element(By.XPATH,
+                                      '//autopay-select[@name="period"]//div[@class="ngx-select__toggle btn form-control"]')
+    time_period.click()
+    tP_option = driver.find_element(By.XPATH, '//span[text()="The last 30 days"]')
+    tP_option.click()
 
+    '''Version for selecting exact data
+    start_time=
+    end_time=
+
+    '''
+    # Apply time period change
+    driver.find_element(By.XPATH, '//button[@type="submit"]').click()
+    time.sleep(1)
+
+    # Downloading invoices
+    directory = ""
+    pages_BTNs = driver.find_elements(By.XPATH, '//div[@class="pagination-pages"]//a')
+    pages = int(len(pages_BTNs) / 2 - 2)  # Divide by 2 because there's 2 divs with page numbers, also -2 for arrows
+
+    for i in range(pages):
+        pages_BTNs[i].click()
+        # get all table of invoices
 
 
 def printInvoices():
-
     return True
 
+
 def main():
-    l=input("Enter your email: ")
-    p=input("Enter your password: ")
+    l = input("Enter your email: ")
+    p = input("Enter your password: ")
 
-    if(login(l,p)):
-        #mode=input("Enter mode: ")
-        printing=input("Do you want to print these invoices? (Y/N): ")
+    if (login(l, p)):
+        # mode=input("Enter mode: ")
+        # printing=input("Do you want to print these invoices? (Y/N): ")
+        getInvoices()
 
-        getInvoices(mode)
+        # if printing=='Y':
+        #    printInvoices()
 
-        if printing=='Y':
-            printInvoices()
-
-        #logoutClose()
+        # logoutClose()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
